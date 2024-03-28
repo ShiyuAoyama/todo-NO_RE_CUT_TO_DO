551da526833537e0c47f54fa7c7b430a6c76c23d
ShiyuAoyama <shiyu.chomeijin@gmail.com>
2024-03-28 21:27:17 +0800

    shiyu_create_profile
    更新、戻るボタンありますが、まだ遷移できません。

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('/css/reset.css')  }}">
+    <link rel="stylesheet" href="{{ asset('/css/profile.css')  }}">
+</head>
+
+{{-- @extends('layouts.app_original')
+@section('content') --}}
+
+<title>nameのプロフィール</title>
+
+<body class="profile_body">
+    {{-- <a href="{{ route('profile') }}">Profile</a> --}}
+    <div class="profile_body_content">
+        @csrf
+        <h1 class="profile_title">Your Profile</h1>
+        <form action="/profile" method="POST" class="profile_input">
+            <label>Your Name</label>
+                <input 
+                name="Name"
+                value="{{ old('Name') }}"
+                type="text">
+            
+            <label>email</label>
+            <input 
+            name="email"
+            value="{{ old('email') }}"
+            type="text">
+
+            <label>password</label>
+            <input 
+            name="password"
+            value="{{ old('password') }}"
+            type="text">
+
+        {{-- <p class="profile_name">name</p>
+        <p class="profile_email">email</p>
+        <p class="profile_password">password</p> --}}
+
+        <button class="profile_back_button" type="button">
+            更新
+        </button>
+        <button class="profile_back_button" type="button">
+            戻る
+        </button>
+        {{-- 戻るボタンでの遷移先 --}}
+        {{-- <form action="{{ route }}"></form> --}}
+        {{-- 更新ボタンでの遷移先 --}}
+        {{-- <form action="{{ route }}"></form> --}}
+        </form>
+    </div>
+    
+</body>
+</html>
+{{-- @endsection --}}
